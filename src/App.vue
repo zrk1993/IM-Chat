0534223fff24978d307994bb34c2916902f7b0e1
renkun <1051455824@qq.com>
2017-05-03 09:57:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <chat></chat>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import chat from './module/chat'
+
+export default {
+  name: 'app',
+  components: {
+    chat,
+    Hello
+  }
+}
+</script>
+
+
